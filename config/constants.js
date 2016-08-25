d05c843e79bc6e34247a8b36cd301b4c90f51dd5
Matt Walters <github@iammattwalters.com>
2016-08-25 13:04:30 -0400

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -4,7 +4,7 @@ module.exports = {
   // we can override this env var to place our PLUGINs in a desired user directory on mac, windows, etc.
   CARDSTACK_PLUGIN_DIR: process.env.CARDSTACK_PLUGIN_DIR 
                         || path.join(process.cwd(), '.plugins'),
-  CARDSTACK_PLUGIN_MANAGER_CMD: process.env.CARDSTACK_PLUGIN_DIR 
+  CARDSTACK_PLUGIN_MANAGER_CMD: process.env.CARDSTACK_PLUGIN_MANAGER_CMD 
                         || path.join(process.cwd(), 'node_modules', 'cardstack-plugin-manager', 'bin', 'cardstack-plugin-manager-run')                        
 
 }
